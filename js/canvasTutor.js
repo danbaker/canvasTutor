8b1d49b4da3dd6211370e97577ba98b4fe5fbf3a
Dan Baker <dan.baker@corda.com>
2011-12-03 11:26:12 -0700

    show Zoom view now

--- js/canvasTutor.js
+++ js/canvasTutor.js
@@ -55,8 +55,41 @@ ut.ct.prototype.paintZoomed = function(idNorm, idZoom) {
 	ctxZoom.clearRect(0,0,wZoom,hZoom);
 	
 	// mult = multipler = cell size (5 means each pixel in norm is a 5x5 pixel in zoom)
-	var mult = wZoom / wNorm;
+	this.mult = wZoom / wNorm;
 	
+	this.normData = ctxNorm.getImageData(0,0,wNorm,hNorm);
+	this.zoomData = ctxZoom.createImageData(wZoom,hZoom);
+	for(var x=0; x<wNorm; x++) {
+		for(var y=0; y<hNorm; y++) {
+			var pix = this.getPixel(x,y);
+			this.setPixel(x,y, pix);
+		}
+	}
+	ctxZoom.putImageData(this.zoomData, 0,0);
+};
+
+ut.ct.prototype.getPixel = function(x,y) {
+	var idx = (x + y * this.normData.width) * 4;
+	return {
+		r:this.normData.data[idx],
+		g:this.normData.data[idx+1],
+		b:this.normData.data[idx+2],
+		a:this.normData.data[idx+3] };
+};
+ut.ct.prototype.setPixel = function(x,y, pix) {
+	x = x * this.mult;
+	y = y * this.mult;
+	var xx;
+	var yy;
+	for(xx=0; xx<this.mult; xx++) {
+		for(yy=0; yy<this.mult; yy++) {
+			var idx = (x+xx + (y+yy) * this.zoomData.width) * 4;
+			this.zoomData.data[idx] = pix.r;
+			this.zoomData.data[idx+1] = pix.g;
+			this.zoomData.data[idx+2] = pix.b;
+			this.zoomData.data[idx+3] = pix.a;
+		}
+	}
 };
 
 // -- -- -- -- -- -- -- -- -- -- --

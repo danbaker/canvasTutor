11c34f1cfe7410cdb224b669080497a9d6a26bed
Dan Baker <dan.baker@corda.com>
2011-12-07 21:24:55 -0700

    kludged parser to function with siple examples

--- js/tutorParser.js
+++ js/tutorParser.js
@@ -29,14 +29,20 @@ ut.tp.prototype.compile = function(js) {
 	JSLINT(this.js, options);
 	var tre = JSLINT.tree;
 	this.processObject(tre);
+	this.addjs("", true, true);
 	console.log("- - - - - - - - - - - - - - - - - - - - - - - -");
 	console.log("- - - - - - - - - - - - - - - - - - - - - - - -");
 	console.log(this.jsCompiled);
 };
 
+ut.tp.prototype.processMain = function(obj) {
+	this.processFirst(obj);
+};
+
+
 ut.tp.prototype.processFirst = function(item) {
 	if (item) {
-		this.log("processFirst");
+//		this.log("processFirst");
 		if (item instanceof Array) {
 			for(var i=0; i<item.length; i++) {
 				this.processObject(item[i]);
@@ -48,11 +54,11 @@ ut.tp.prototype.processFirst = function(item) {
 };
 ut.tp.prototype.processSecond = function(item) {
 	if (item) {
-		this.log("processSecond");
+//		this.log("processSecond");
 		if (item instanceof Array) {
 			for(var i=0; i<item.length; i++) {
 				if (i > 0) {
-					this.log("second: ,");
+//					this.log("second: ,");
 					this.addjs(", ");
 				}
 				this.processObject(item[i]);
@@ -64,14 +70,15 @@ ut.tp.prototype.processSecond = function(item) {
 };
 
 ut.tp.prototype.processObject = function(obj) {
+	if (obj.edge) this.processEdge(obj);
 	this.depth++;
-		this.log("processObject");
+//		this.log("processObject");
 	if (obj.line < this.lineOn) {
 		console.log("LINE NUMBER WEIRDAGE");
 	} else if (obj.line > this.lineOn) {
-		this.addjs("", true);
+//		this.addjs("", true);
 		this.lineOn = obj.line;
-		this.log("--- line "+ obj.line + "---");
+		this.log("------ line "+ obj.line + "------");
 	}
 	switch (obj.arity) {
 	case undefined:
@@ -99,23 +106,11 @@ ut.tp.prototype.processObject = function(obj) {
 		console.log("NEW ARITY NOT HANDLED: "+obj.arity);
 		break;
 	}
-	if (obj.block) {
-		this.depth++;
-		this.blockDepth++;
-		this.log("BLOCK");
-		this.addjs(" {");
-		this.processFirst(obj.block);
-		this.blockDepth--;
-		this.addjs("", true);
-		this.addjs("}");
-		this.depth--;
-	}
 	this.depth--;
-	if (obj.edge) this.log("EDGE2");
 };
 
 ut.tp.prototype.processUndefined = function(obj) {
-		this.log("processUndefined");
+//		this.log("processUndefined");
 	if (obj.first) {
 		this.processFirst(obj.first);
 	}
@@ -123,48 +118,85 @@ ut.tp.prototype.processUndefined = function(obj) {
 };
 
 ut.tp.prototype.processNumber = function(obj) {
-		this.log("processNumber");
+//		this.log("processNumber");
 	this.processObjectValue(obj, "number");
 	this.processFirst(obj.first);
 	this.processSecond(obj.second);
 };
 ut.tp.prototype.processString = function(obj) {
-		this.log("processString");
+//		this.log("processString");
 	this.processObjectValue(obj, "string");
 	this.processFirst(obj.first);
 	this.processSecond(obj.second);
 };
 
 ut.tp.prototype.processStatement = function(obj) {
-		this.log("processStatement");
+//	this.log("processStatement.start");
+	if (obj.first && obj.second && obj.third) {
+		this.processStatement_3(obj);
+	} else if (obj.first && obj.second) {
+		this.processStatement_2(obj);
+	} else if (obj.first) {
+		this.processStatement_1(obj);
+	} else {
+		this.processStatement_0(obj);
+	}
+};
+ut.tp.prototype.processStatement_3 = function(obj) {
+	this.processObjectValue(obj);
+	this.addjs("(");
+	this.processFirst(obj.first);
+	this.addjs("; ");
+	this.processSecond(obj.second);
+	this.addjs("; ");
+	this.processFirst(obj.third);
+	this.addjs(")");
+	this.processBlock(obj.block);
+};
+ut.tp.prototype.processStatement_1 = function(obj) {
 	this.processObjectValue(obj);
 	this.addjs(" ");
 	this.processFirst(obj.first);
-	this.processSecond(obj.second);
-	this.processFirst(obj.third);
-//	this.jsCompiled += ";"; ?? What about the "for" block ... ???
+	this.addjs(";");
+};
+
+ut.tp.prototype.processBlock = function(block) {
+	if (block) {
+		this.depth++;
+		this.blockDepth++;
+//		this.log("BLOCK");
+		this.addjs(" {");
+		this.processMain(block);
+		this.blockDepth--;
+		this.addjs("", true);
+		this.addjs("}");
+		this.depth--;
+	}
 };
 
 ut.tp.prototype.processPrefix = function(obj) {
-		this.log("processPrefix");
+//		this.log("processPrefix");
 	this.processObjectValue(obj);
 	this.processFirst(obj.first);
 	this.processSecond(obj.second);
 };
 ut.tp.prototype.processInfix = function(obj) {
-		this.log("processInfix");
+//		this.log("processInfix");
 	this.processFirst(obj.first);
 	this.processObjectValue(obj);
 	this.processSecond(obj.second);
 };
 ut.tp.prototype.processSuffix = function(obj) {
-		this.log("processSuffix");
+//		this.log("processSuffix");
 	this.processFirst(obj.first);
 	this.processSecond(obj.second);
 	this.processObjectValue(obj);
 };
 
 ut.tp.prototype.processObjectValue = function(obj, type) {
+	if (obj.number === undefined && (obj.string === undefined || obj.string === '(begin)')) {
+		return;
+	}
 	if (!type) {
 		if(typeof(obj.number) === "number") { type = "number";
 		} else if (typeof(obj.string) === "string") { type = "string";
@@ -180,7 +212,10 @@ ut.tp.prototype.processObjectValue = function(obj, type) {
 	} else {
 		this.log("ERROR: No String or Number");
 	}
-	if (obj.edge) this.log("EDGE");
+};
+
+ut.tp.prototype.processEdge = function(txt) {
+	this.addjs("", true, true);
 };
 
 ut.tp.prototype.log = function(txt) {
@@ -190,12 +225,20 @@ ut.tp.prototype.log = function(txt) {
 };
 
 
-ut.tp.prototype.addjs = function(txt, eol) {
+ut.tp.prototype.addjs = function(txt, eol, isEdge) {
 	this.jsCompiled += txt;
 	if (eol) {
 		if (this.openParen) {
 			this.openParen = false;
 			this.jsCompiled += ");";
+		}
+		if (isEdge) {
+			// KLUDGE
+			var c = this.jsCompiled.substring(this.jsCompiled.length-1);
+			if (c != ';' && c != '{') {
+				this.jsCompiled += ';';
+			}
+//			this.jsCompiled += "//EDGE";
 		}
 		this.jsCompiled += "\r\n";
 		for(var i=0; i<this.blockDepth; i++) {
